51b71c8d09563bc0f1122af8a7094f6c3f4e5e84
byu777 <byu@bcit.ca>
2021-10-17 14:52:14 -0700

    Initial Commit

---
+++ FortuneCookies/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FortuneCookies.Models;
+
+namespace FortuneCookies.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.cookie = Models.FortuneCookies.getCookie();
+            return View();
+        }
+    }
+}
---
+++ FortuneCookies/Models/FortuneCookies.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FortuneCookies.Models
+{
+    public class FortuneCookies
+    {
+        static List<string> Cookies = new List<string>
+        {
+            "You are going to get married!",
+            "You will find a new job!",
+            "You will make new friends!",
+            "You will be successful!",
+            "You will make a lot of money"
+        };
+
+        public static string getCookie()
+        {
+
+            var random = new Random();
+            int index = random.Next(Cookies.Count);
+            return Cookies[index];
+
+        }
+    }
+}
